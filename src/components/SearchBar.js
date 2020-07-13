5f85e83b746a5ccd7fe3febde2fc113153f040cd
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-13 16:02:19 -0600

    input field styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Feather name='search' size={30} />
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name='search' style={styles.iconStyle} />
+      <TextInput style={styles.inputStyle} placeholder='Search' />
     </View>
   )
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
+    marginTop: 15,
     backgroundColor: '#F0EEEA',
     height: 50,
     borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
     marginHorizontal: 15
   }
 });
