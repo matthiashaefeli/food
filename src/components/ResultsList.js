a6ffbe296718340888d138dbed0fb4241736083f
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-16 16:14:13 -0600

    last changes, git reverse

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,7 +4,7 @@ import ResultsDetail from '../components/ResultsDetail';
 
 const ResultsList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal={true}
@@ -23,7 +23,12 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 10
   }
 });
 
